613ca8adbfc417b14bc4099a59035b327c8ff8dc
Luis Fonseca <01luisfonseca@gmail.com>
2017-01-26 09:30:54 -0500

    Ajuste de softdelete a Generales

--- app/Generales.php
+++ app/Generales.php
@@ -2,13 +2,11 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Softdeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Generales extends Model
 {
     //
-    use Softdeletes;
     protected $table = 'generales';
 
 }
