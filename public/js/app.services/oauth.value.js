3eb431437c78a346bcdde8912c32e26899bde5cb
Luis Fonseca <01luisfonseca@gmail.com>
2017-03-27 08:32:26 -0500

    Ajuste de Llave

--- public/js/app.services/oauth.value.js
+++ public/js/app.services/oauth.value.js
@@ -2,10 +2,10 @@
 	'use strict';
 	angular.module('escuela')
 	.constant('OAUTHINFO',{
-		URLVERIFY:'/oauth/token',
-		URLREFRESH:'/oauth/token',
+		URLVERIFY:'oauth/token',
+		URLREFRESH:'oauth/token',
 		CLIENTID:3,
-		CLIENTSECRET:'I8dVQ8umBnjfkrutVB6suAeHMbjr2nVUGRmNjGOn',
+		CLIENTSECRET:'PdH2ikQTQPaXF8ilFs6CsZbmYyUBaReZfNXujVqT',//'I8dVQ8umBnjfkrutVB6suAeHMbjr2nVUGRmNjGOn',
 		GRANTTYPEREQUEST:'password',
 		GRANTTYPEREFRESH:'refresh_token'
 	});
