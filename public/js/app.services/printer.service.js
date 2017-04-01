7a102bcbb48f82b90df8e5cf8cde8e5cc6202947
Luis Fonseca <01luisfonseca@gmail.com>
2017-04-01 11:08:21 -0500

    Finalizacion de boletines y ajustes de otros

--- public/js/app.services/printer.service.js
+++ public/js/app.services/printer.service.js
@@ -9,7 +9,7 @@
 		// Funciones
 		vm.div=div;
 
-		function div(div){
+		function div(div,otherlink,cb){
 	      var printContents = document.getElementById(div).innerHTML;
 	      var newWindow = window.open("", "newWindow", "");
 	      newWindow.document.write('<link rel="stylesheet" href="/js/node_modules/bootstrap/dist/css/bootstrap.min.css">');
@@ -17,9 +17,16 @@
 	      newWindow.document.write('<link rel="stylesheet" href="/css/app.css">');
 	      newWindow.document.write('<link rel="stylesheet" href="/js/node_modules/ng-dialog/css/ngDialog.min.css">');
 	      newWindow.document.write('<link rel="stylesheet" href="/js/node_modules/ng-dialog/css/ngDialog-theme-default.min.css">');
+	      if (typeof otherlink!=='undefined') {
+	      	for (var i = 0; i < otherlink.length; i++) {
+	      		newWindow.document.write(otherlink[i]);
+	      	}
+	      }
 	      newWindow.document.write(printContents);
 	      setTimeout(function() {
 	      	newWindow.print();
+	      	newWindow.close();
+	      	if(typeof cb==='function') cb(true);
 	      }, 10);
 		}
 	}
--- public/js/boletines/controller.js
+++ public/js/boletines/controller.js
@@ -16,6 +16,7 @@
 		vm.anios=[];
 		vm.notas=[];
 		vm.notastabla=[];
+		vm.periodosCurso=[]; // Para los Stats por curso
 		vm.gen={}; // Titulo y otros de la tabla
 		vm.sel={
 			anio: -1,
@@ -30,6 +31,7 @@
 		vm.nivelSel=nivelSel;// Verifica si se hizo seleccion de nivel
 		vm.hayInfoAlumnos=hayInfoAlumnos;// Verifica si hay la información de alumnos para crear boletines
 		vm.lanzaBoletin=lanzaBoletin;// Lanza los boletines listos para la impresión
+		vm.calcPromGen=calcPromGen; // Calcula el promedio de una lista de promedios.
 
 		// Lanzamiento Automático
 		animPage.show('boletines',function(){});
@@ -56,9 +58,47 @@
  			return vm.notastabla.length>0;
  		}
  		function lanzaBoletin(id){
- 			console.log(opcion.get('Logo'));
- 			console.log(opcion.get('Slogan'));
- 			console.log(id);
+ 			if(id===0){
+ 				//console.log(vm.notastabla);
+ 				let varios=JSON.parse(JSON.stringify(vm.notastabla));
+ 				imprimeVariosBol(varios);
+ 			}else{
+ 				imprimeBol(id);
+ 			}
+
+ 			function imprimeBol(index,cb){
+ 				let miestilo=[
+ 				'<link rel="stylesheet" href="css/boletin.css">'
+	 			];
+	 			printer.div('prinZoneBoletines'+index,miestilo,cb);
+	 			return true;
+ 			}
+
+ 			function imprimeVariosBol(arr){
+ 				imprimeBol(arr[0].id,(st)=>{
+ 					if (!st) {
+ 						console.error('No se imprime boletin por error.',arr[0].id)
+ 						return false;
+ 					}
+ 					arr.shift();
+ 					if(arr.length>0){
+ 						imprimeVariosBol(arr);
+ 					}else{
+ 						return true;
+ 					}
+ 				});
+ 			}
+ 		}
+ 		function calcPromGen(elm,pto){
+ 			if (!pto) pto='prom';
+ 			var prom=0, cta=0;
+ 			for (var i = 0; i < elm.length; i++) {
+ 				if (vm.periodosCurso[i].cuenta>0) {
+ 					prom += elm[i][pto];
+ 					cta++;
+ 				}
+ 			}
+ 			return prom/(cta || 1);
  		}
  		function getAnios(){
  			$rootScope.$broadcast('cargando',true);
@@ -96,26 +136,62 @@
 
  			function armarTabla(nt){
  				for (var i = 0; i < nt.alumnos.length; i++) {
- 					var acum=0;
- 					for (var j = 0; j < nt.alumnos[i].materias.length; j++) {
- 						for (var k = 0; k < nt.alumnos[i].materias[j].periodo.length; k++) {
- 							if(nt.alumnos[i].materias[j].periodo[k].nombre==vm.anios[vm.sel.anio].periodos[vm.sel.pers].nombre){
- 								acum += nt.alumnos[i].materias[j].periodo[k].prom;
- 							}
+ 					nt.alumnos[i].periodos=[]; // Se añaden periodos al alumno
+ 					if(nt.alumnos[i].materias){
+ 						// Para guardar todos los periodos en el nuevo arreglo
+ 						for (var x = 0; x < nt.alumnos[i].materias[0].periodo.length; x++) {
+ 							nt.alumnos[i].periodos.push({
+ 								nombre: nt.alumnos[i].materias[0].periodo[x].nombre,
+ 								puesto: 0,
+ 								prom: 0
+ 							});
  						}
  					}
- 					acum /= nt.alumnos[i].materias.length;
- 					nt.alumnos[i].promPer=acum;
+ 					for (var j = 0; j < nt.alumnos[i].materias.length; j++) {
+ 						for (var k = 0; k < nt.alumnos[i].materias[j].periodo.length; k++) {
+ 							// Acumulamos las notas para el promedio por periodo por alumno
+ 							nt.alumnos[i].periodos[k].prom += nt.alumnos[i].materias[j].periodo[k].prom; 
+ 						}
+ 					}
+ 					for (var x = 0; x < nt.alumnos[i].periodos.length; x++) {
+ 						// Promedio global por periodo, por alumno
+ 						nt.alumnos[i].periodos[x].prom /= nt.alumnos[i].materias.length || 1;
+ 					}
  				}
- 				nt.alumnos.sort((a,b)=>{
- 					return b.promPer - a.promPer;
- 				});
- 				for (var i = 0; i < nt.alumnos.length; i++) {
- 					nt.alumnos[i].puestoCurso=i+1;
+ 				for (var r = 0; r < nt.alumnos[0].periodos.length; r++) {
+ 					// Inicializamos los promedios globales por curso
+ 					vm.periodosCurso.push({
+ 						nombre: nt.alumnos[0].periodos[r].nombre,
+ 						cuenta:0,
+ 						prom:0
+ 					});
+ 					// Ordenamos al alumno segun el periodo y el orden
+ 					nt.alumnos.sort((a,b)=>{
+	 					return b.periodos[r].prom - a.periodos[r].prom;
+	 				});
+	 				for (var i = 0; i < nt.alumnos.length; i++) {
+	 					vm.periodosCurso[r].cuenta += nt.alumnos[i].periodos[r].prom;
+	 				}
+	 				for (var x = 0; x < nt.alumnos.length; x++) {
+	 					if (vm.periodosCurso[r].cuenta>0){
+	 						nt.alumnos[x].periodos[r].puesto=x+1;
+	 					}
+	 				}
  				}
+ 				for (var i = 0; i < vm.periodosCurso.length; i++) {
+ 					for (var x = 0; x < nt.alumnos.length; x++) {
+ 						vm.periodosCurso[i].prom += nt.alumnos[x].periodos[i].prom;
+ 					}
+ 					vm.periodosCurso[i].prom /= nt.alumnos.length || 1;
+ 				}
+ 				llenarInfo();
  				vm.notastabla=nt.alumnos; // Alumnos por nivel ordenados
- 				console.log(vm.notastabla); // Para eliminar
  			}
  		}	
+ 		function llenarInfo(){
+ 			vm.info.logo=opcion.get('Logo');
+			vm.info.slogan=opcion.get('Slogan');
+			vm.info.org=opcion.get('Organización');
+ 		}
 	}
 })();
