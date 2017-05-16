a04158be5e0588ce7ef7e32f590d3abefd87992c
Luis Fonseca <01luisfonseca@gmail.com>
2017-05-15 22:22:05 -0500

    Ajustes de gráficas hijas

--- public/js/cursymat/controller.js
+++ public/js/cursymat/controller.js
@@ -9,6 +9,7 @@
 
 		// Variables básicas
 		vm.anios=[];
+		vm.muestraboton=false;
 		vm.sel={
 			anio: 0,
 			per: null,
@@ -95,8 +96,10 @@
  			});
  		}
  		function mostrarGraficos(){
+ 			vm.muestraboton=false;
  			for (var i = 0; i < vm.status.cur.name.length; i++) {
  				vm.status.cur.name[i]= reductNames(vm.status.cur.name[i]);
+ 				vm.status.cur.filtroMateria[i].reducName=vm.status.cur.name[i];
  			}
  			for (var j = 0; j < vm.status.cur.filtroMateria.length; j++) {
 				var fmat=vm.status.cur.filtroMateria[j];
@@ -106,6 +109,7 @@
 			}
 			for (var i = 0; i < vm.status.mat.name.length; i++) {
  				vm.status.mat.name[i]= reductNames(vm.status.mat.name[i]);
+ 				vm.status.mat.promCurso[i].reducName = vm.status.mat.name[i];
  			}
  			for (var j = 0; j < vm.status.mat.promCurso.length; j++) {
 				var fmat=vm.status.mat.promCurso[j];
@@ -114,10 +118,31 @@
 				}
 			}
  			if (vm.sel.type=='mat') {
- 				dibujaGrafico('Rendimiento académico por Materia.',vm.status.mat);
+ 				dibujaGrafico('Rendimiento académico por Materia.',vm.status.mat, nuevoGraficoMat);
  			}else{
- 				dibujaGrafico('Rendimiento académico por Curso.',vm.status.cur);
+ 				dibujaGrafico('Rendimiento académico por Curso.',vm.status.cur, nuevoGraficoCur);
  			}
+ 		}
+ 		function nuevoGraficoMat(dt){
+ 			vm.muestraboton=true;
+ 			let dato= dt.dataPoint.label;
+ 			let id=posName(dato,'mat','promCurso');
+ 			if(id!==null) dibujaGrafico('Rendimiento de '+vm.status.mat.promCurso[id].mat+' según los cursos.',vm.status.mat.promCurso[id]);
+ 		}
+ 		function nuevoGraficoCur(dt){
+ 			vm.muestraboton=true;
+ 			let dato= dt.dataPoint.label;
+ 			let id= posName(dato,'cur','filtroMateria');
+ 			if(id!==null) dibujaGraficoFull('Rendimiento de '+vm.status.cur.filtroMateria[id].cur+' según las materias.',vm.status.cur.filtroMateria[id]);
+ 		}
+ 		function posName(dt,tipo,array){
+ 			let dato=dt;
+ 			let srcharr=vm.status[tipo][array];
+ 			for (var i = 0; i < srcharr.length; i++) {
+ 				let matching=srcharr[i].reducName;
+ 				if(matching==dato) return i;
+ 			}
+ 			return null;
  		}
  		function reductNames(name){
  			if(name.indexOf(' ')>-1){
@@ -125,9 +150,9 @@
  				return splitted[0].substring(0,3)+'. '+splitted[splitted.length-1].substring(0,5);
  			}else{
  				if (name.indexOf('-')>-1) {
- 					return name.substring(0,9);
+ 					return name.substring(0,9); // Si tiene un separador es una palabra compuesta
  				}
- 				return name.substring(0,5); // Una palabra es recortada a 5 letras
+ 				return name.substring(0,7); // Una palabra es recortada a 7 letras
  			}
  		}
  		function calcularDatos(dts){
@@ -178,6 +203,7 @@
  				vm.status.cur.name.push(dts[i].niveles.nombre); // Nombre Niveles
  				vm.status.cur.value.push(dts[i].prom); // Promedio por nivel.
  				vm.status.cur.filtroMateria.push({
+ 					cur:dts[i].niveles.nombre,
  					nivelid:dts[i].id,
  					posmats: mats, // Posiciones de materias
  					name: matname,
@@ -195,6 +221,7 @@
 				vm.status.mat.name.push('');
 				vm.status.mat.value.push(0);
 				vm.status.mat.promCurso.push({
+					mat:'',
 					posmats: [],
 					name:[],
 					value:[],
@@ -204,6 +231,7 @@
 						cuentael ++;
 						vm.status.mat.name[i]=materiasDisponibles[j].nombre;
 						vm.status.mat.value[i] += materiasDisponibles[j].prom;
+						vm.status.mat.promCurso[i].mat=materiasDisponibles[j].nombre;
 						vm.status.mat.promCurso[i].posmats.push(materiasDisponibles[j].posmats);
 						vm.status.mat.promCurso[i].name.push(materiasDisponibles[j].curso);
 						vm.status.mat.promCurso[i].value.push(materiasDisponibles[j].prom);
@@ -259,6 +287,52 @@
             function formatter(e) {
 					return parseFloat(e.dataPoint.y).toFixed(2);
 			}
+ 		}
+ 		function dibujaGraficoFull(title,data,callback,container){
+ 			if (typeof callback=='undefined') {
+ 				callback=function(e){
+ 					console.log(e);
+ 				};
+ 			}
+ 			if (typeof container=='undefined') {
+ 				container="chartContainer";
+ 			}
+ 			var datapointPasan=[], datapointNoPasan=[];
+ 			for (var i = 0; i < data.name.length; i++) {
+ 				datapointPasan.push({
+ 					y: data.value[i],
+ 					label: data.name[i],
+ 					//indexLabel: data.value[i],
+ 				});
+ 				datapointNoPasan.push({
+ 					y: data.value2[i],
+ 					label: data.name[i],
+ 					//indexLabel: data.value[i],
+ 				});
+ 			}
+ 			console.log(datapointPasan,datapointNoPasan);
+ 			$window.chart2 = new CanvasJS.Chart(container, {
+ 				title:{
+        			text: title
+     			},
+            	data: [
+                	{
+                		//click: callback,
+                		type: "stackedColumn",
+						//showInLegend: true, 
+						name: "Pasan",
+						dataPoints: datapointPasan
+                	},
+                	{
+                		//click: callback,
+                		type: "stackedColumn",
+						//showInLegend: true, 
+						name: "No pasan",
+						dataPoints: datapointNoPasan
+                	}
+                ]
+            });
+            $window.chart2.render();
  		}
 		
 	}
